ac1da3dd9b0bd1d1f919feaf94b972766c86f972
closada <38966204+closada@users.noreply.github.com>
2020-05-21 13:48:11 -0300

    Add files via upload

---
+++ 02-EjemploTipos/EjemploTipos.cpp
@@ -0,0 +1,33 @@
+/* Camila Victoria Losada
+20200518
+Tipos de Datos */
+
+#include <iostream>
+#include <string.h>
+#include <cassert>
+
+int main() {
+/* ejemplo de un tipo de dato double- numero real-*/
+double numero{2.53};
+
+/* ejemplo de cadena de texto string y de caracter */
+std::string cadena = "Cadena de Texto";
+char caracter{'C'};
+
+/********************/
+bool booleano{true};
+unsigned contador{1};
+int i{-3};
+
+/* en este otro ejemplo, se puede ver la utilización de los tipos unsigned, bool, int */
+if (booleano == true)
+{
+for (contador;contador<=2;contador++)
+{
+i = i + 1;
+}}
+assert(i == -1);
+assert(caracter == 'C');
+assert(cadena == "Cadena de Texto");
+assert(numero == 2.53);
+}
