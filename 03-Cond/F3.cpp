064b1ea36b039815475d0fd5e7531f9584f70f0e
closada <38966204+closada@users.noreply.github.com>
2020-05-31 21:54:05 -0300

    Update F3.cpp

--- 03-Cond/F3.cpp
+++ 03-Cond/F3.cpp
@@ -4,17 +4,17 @@ Funciones y Operador Condicional */
 
 #include <cassert>
 
-double funcion (double);
+double F3 (double);
 
 int main () {
 
-assert(-3 == funcion(3));
-assert(3.1 == funcion(3.1));
-assert(3 == funcion(-3));
-assert(4.1 == funcion(4.1));
-assert(2 == funcion(-2));
-assert(-1 == funcion(1));
+assert(-3 == F3(3));
+assert(3.1 == F3(3.1));
+assert(3 == F3(-3));
+assert(4.1 == F3(4.1));
+assert(2 == F3(-2));
+assert(-1 == F3(1));
 
 }
 
-double funcion(double x){return x>=-3 and x<=3 ? -x : x;}
+double F3(double x){return x>=-3 and x<=3 ? -x : x;}
