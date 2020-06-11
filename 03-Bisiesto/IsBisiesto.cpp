d175c998c595b5ed3fcd64bd0a1a1975ac29b2dc
closada <38966204+closada@users.noreply.github.com>
2020-06-11 19:55:47 -0300

    Update IsBisiesto.cpp

--- 03-Bisiesto/IsBisiesto.cpp
+++ 03-Bisiesto/IsBisiesto.cpp
@@ -9,18 +9,19 @@ bool IsBisiesto (unsigned);
 int main() {
 
 assert (true == IsBisiesto(2020));
-assert (true == IsBisiesto(2000));
-assert (true == IsBisiesto(1600));
-assert (true == IsBisiesto(2400));
-assert (true == IsBisiesto(2016));
+assert (IsBisiesto(2000));
+assert (IsBisiesto(1600));
+assert (IsBisiesto(2400));
+assert (IsBisiesto(2016));
+assert (IsBisiesto(1996));
 
-assert (false == IsBisiesto(1582));
-assert (false == IsBisiesto(2021));
-assert (false == IsBisiesto(1700));
-assert (false == IsBisiesto(1800));
-assert (false == IsBisiesto(1900));
-assert (false == IsBisiesto(2100));
+assert (not IsBisiesto(1582));
+assert (not IsBisiesto(2021));
+assert (not IsBisiesto(1700));
+assert (not IsBisiesto(1800));
+assert (not IsBisiesto(1900));
+assert (not IsBisiesto(2100));
 }
 
 bool IsBisiesto (unsigned anio) {return 
-anio>=1582 and anio%4==0 and(anio%100!=0 or (anio%100==0 and anio%400==0)) ? true : false;}
+anio>=1582 and anio%4==0 and(anio%100!=0 or (anio%100==0 and anio%400==0));}
